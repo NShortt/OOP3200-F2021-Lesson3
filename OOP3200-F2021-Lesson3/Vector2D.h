e4a9305173792a482de1a60395690bb45949fe93
Nicholas Shortt <Nicholas.Shortt@hotmail.com>
2021-09-20 09:17:32 -0400

    Add project files.

---
+++ OOP3200-F2021-Lesson3/Vector2D.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef __VECTOR_2D__
+#define __VECTOR_2D__
+
+class Vector2D
+{
+public:
+	// CONSTRUCTOR(S)
+
+	// DESTRUCTOR
+
+	// ACCESSORS
+
+	// MUTATORS
+
+	// PUBLIC METHODS
+
+private:
+	// ATTRIBUTES
+
+	// PRIVATE METHODS
+};
+
+#endif /* (__VECTOR_2D__) */
+
---
+++ OOP3200-F2021-Lesson3/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Author: Nicholas Shortt
+ * StudentID: 100630003
+ *
+ * Date: September 20, 2021
+ *
+ * Desc: Demo porject for OOP3200 - F2021- Week 3
+ *
+ */
+
+#include <iostream>
+
+
+int main()
+{
+	
+	std::cout << "Hello World!\n";
+}
+
